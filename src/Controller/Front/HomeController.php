61f8cf98650576be98091e068425809be0e94d65
unknown <redha955@hotmail.fr>
2019-01-15 20:14:27 +0100

    Ajout barre de recherche + filtres de recherches
    Signed-off-by: unknown <redha955@hotmail.fr>

--- src/Controller/Front/HomeController.php
+++ src/Controller/Front/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Front;
 
+use App\Repository\PlatformRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,10 +14,9 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="home")
      */
-    public function index()
+    public function index(PlatformRepository $platformRepository) : \Symfony\Component\HttpFoundation\Response
     {
-        return $this->render('Front/home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('Front/home/index.html.twig', ['platforms' => $platformRepository->findAll()]);
     }
+
 }
