413fd93a9078a87bfb34081681c1650e675cec3f
OmarAbdallaa <omar.abdallaa@gmail.com>
2019-01-16 10:15:06 +0100

    editor v2
    Signed-off-by: OmarAbdallaa <omar.abdallaa@gmail.com>

--- src/Form/EditorType.php
+++ src/Form/EditorType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 
 class EditorType extends AbstractType
@@ -14,7 +15,9 @@ class EditorType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class, [
+                "label" => "Nom",
+            ])
             ->add('imgFile', FileType::class, [
                 "label" => "Logo",
                 "required" => false
