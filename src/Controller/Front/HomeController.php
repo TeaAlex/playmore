6beea6fecef43b51df1dd6b8070428503b84c638
unknown <redha955@hotmail.fr>
2019-01-16 09:55:38 +0100

    ajout des filtres
    Signed-off-by: unknown <redha955@hotmail.fr>

--- src/Controller/Front/HomeController.php
+++ src/Controller/Front/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Front;
 
+use App\Repository\CategoryRepository;
 use App\Repository\PlatformRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,9 +15,10 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="home")
      */
-    public function index(PlatformRepository $platformRepository) : \Symfony\Component\HttpFoundation\Response
+    public function index(PlatformRepository $platformRepository,CategoryRepository $categoryRepository) : \Symfony\Component\HttpFoundation\Response
     {
-        return $this->render('Front/home/index.html.twig', ['platforms' => $platformRepository->findAll()]);
+        return $this->render('Front/home/index.html.twig', ['platforms' => $platformRepository->findAll(),
+            'categories' => $categoryRepository->findAll()]);
     }
 
 }
