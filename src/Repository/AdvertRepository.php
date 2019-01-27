ebc4cbb95ed4ece409e362f2260babe284f4797b
Alex Tea <alextea2@gmail.com>
2019-01-27 17:01:12 +0100

    fix query

--- src/Repository/AdvertRepository.php
+++ src/Repository/AdvertRepository.php
@@ -30,18 +30,24 @@ class AdvertRepository extends ServiceEntityRepository
     	$rsm->addScalarResult('game_owned_id','game_owned_id');
     	$rsm->addScalarResult('game_owned_name','game_owned_name');
     	$rsm->addScalarResult('game_owned_img_name','game_owned_img_name');
+    	$rsm->addScalarResult('game_owned_platform','game_owned_platform');
     	$rsm->addScalarResult('game_wanted_id','game_wanted_id');
     	$rsm->addScalarResult('game_wanted_name','game_wanted_name');
     	$rsm->addScalarResult('game_wanted_img_name','game_wanted_img_name');
+    	$rsm->addScalarResult('game_wanted_platform','game_wanted_platform');
     	$rsm->addScalarResult('username','username');
 		$sql = <<<SQL
-			SELECT a.id advert_id, ak.id advert_kind_id, a.start_date, a.end_date, a.price, g.id game_owned_id,  g.name game_owned_name, g.img_name game_owned_img_name,
-       		a.game_wanted_id game_wanted_id, i2.name game_wanted_name, i2.img_name game_wanted_img_name, u.username
+			SELECT a.id advert_id, a.advert_kind_id , a.start_date, a.end_date, a.price, u.username,
+			       g.id game_owned_id, g.name game_owned_name, g.img_name game_owned_img_name, p.name game_owned_platform,
+       			   g2.id game_wanted_id, g2.name game_wanted_name, g2.img_name game_wanted_img_name, p2.name game_wanted_platform
 			FROM advert a
-			JOIN advert_kind ak ON a.advert_kind_id = ak.id
-			JOIN game g ON a.game_owned_id = g.id
+			JOIN game_platform gp ON a.game_owned_id = gp.id
+			JOIN game g ON gp.game_id = g.id
+			JOIN platform p ON gp.platform_id = p.id
+			LEFT JOIN game_platform gp2 ON a.game_wanted_id = gp2.id
+			LEFT JOIN game g2 ON gp2.game_id = g2.id
+		  	LEFT JOIN platform p2 ON gp2.platform_id = p2.id
 			JOIN user u ON a.created_by_id = u.id
-			LEFT JOIN game i2 ON a.game_wanted_id = i2.id
 SQL;
 		return $this->getEntityManager()->createNativeQuery($sql, $rsm)->getResult();
 
