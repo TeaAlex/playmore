8f8ca57c40a1f32d9c8dd491d7ec0745bfbc26dd
Alex Tea <alextea2@gmail.com>
2019-05-18 00:41:28 +0200

    wip

--- config/bundles.php
+++ config/bundles.php
@@ -20,5 +20,4 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     FOS\JsRoutingBundle\FOSJsRoutingBundle::class => ['all' => true],
-    Bazinga\GeocoderBundle\BazingaGeocoderBundle::class => ['all' => true],
 ];
--- src/Controller/Front/HomeController.php
+++ src/Controller/Front/HomeController.php
@@ -2,18 +2,19 @@
 
 namespace App\Controller\Front;
 
-use App\Entity\Advert;
+use Algolia\SearchBundle\IndexManagerInterface;
 use App\Repository\AdvertRepository;
 use App\Repository\CategoryRepository;
 use App\Repository\PlatformRepository;
-use Geocoder\Plugin\PluginProvider;
-use Geocoder\ProviderAggregator;
+use Geocoder\Provider\AlgoliaPlaces\AlgoliaPlaces;
 use Geocoder\Query\GeocodeQuery;
+use Geocoder\Query\ReverseQuery;
+use Geocoder\StatefulGeocoder;
+use Http\Adapter\Guzzle6\Client;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\Common\Persistence\ObjectManager;
 
 /**
 * @Route(name="app_security_")
@@ -27,8 +28,20 @@ class HomeController extends AbstractController
 	 *
 	 * @return Response
 	 */
-    public function index(Request $request,  PlatformRepository $platformRepository,CategoryRepository $categoryRepository) : Response
+    public function index(Request $request, IndexManagerInterface $indexingManager, PlatformRepository $platformRepository,CategoryRepository $categoryRepository) : Response
     {
+        if($this->getUser()){
+            $user = $this->getUser();
+            $latlong = "{$user->getLat()}, {$user->getLon()}";
+            $curl = curl_init();
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($curl, CURLOPT_URL, "https://places-dsn.algolia.net/1/places/reverse?aroundLatLng={$latlong}&hitsPerPage=1&language=fr");
+            curl_setopt($curl, CURLOPT_HEADER, "X-Algolia-Application-Id: {$_ENV['ALGOLIA_APP_ID']}");
+            curl_setopt($curl, CURLOPT_HEADER, "X-Algolia-API-Key: {$_ENV['ALGOLIA_API_KEY']}");
+            $res = json_decode(curl_exec($curl), true);
+            dump($res);
+        }
+
         return $this->render('Front/home/index.html.twig', ['platforms' => $platformRepository->findAll(),
             'categories' => $categoryRepository->findAll()]);
     }
