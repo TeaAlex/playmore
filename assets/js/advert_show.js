93f57f71a640abb93bbd80db5c060d976d039eb9
Alex Tea <alextea2@gmail.com>
2019-05-27 23:46:39 +0800

    add condition

--- assets/js/advert_show.js
+++ assets/js/advert_show.js
@@ -5,55 +5,58 @@ const offerBtn = document.querySelector('#offer-btn');
 const backdrop = document.querySelector('.backdrop');
 const modal = document.querySelector('.modal');
 
-offerBtn.addEventListener('click', (e) => {
-    backdrop.style.display = 'block';
-    modal.style.display = 'block';
-});
+if(offerBtn !== null){
+    offerBtn.addEventListener('click', (e) => {
+        backdrop.style.display = 'block';
+        modal.style.display = 'block';
+    });
 
-backdrop.addEventListener('click', (e) => {
-    backdrop.style.display = 'none';
-    modal.style.display = 'none';
-});
+    backdrop.addEventListener('click', (e) => {
+        backdrop.style.display = 'none';
+        modal.style.display = 'none';
+    });
 
-const acceptBtns = document.querySelectorAll('.accept-btn');
-acceptBtns.forEach((btn) => {
-    btn.addEventListener('click', function(e) {
-        const id = this.dataset.offerId;
-        fetch(Routing.generate('offer_accept', {'id': id}), {
-            'method': 'POST'
-        })
-            .then(res => res.json())
-            .then(offers => {
-                offers.forEach(declineOffer(id));
-                const card = document.querySelector(`div[data-offer-id="${id}"]`);
-                console.log(card);
-                const buttons = card.querySelector('.buttons');
-                card.removeChild(buttons);
-                const p = document.createElement('p');
-                p.innerText = 'offre accepté';
-                p.classList.add('text-right');
-                p.classList.add('font-bold');
-                p.classList.add('text-green-light');
-                p.classList.add('uppercase');
-                card.appendChild(p);
+    const acceptBtns = document.querySelectorAll('.accept-btn');
+    acceptBtns.forEach((btn) => {
+        btn.addEventListener('click', function(e) {
+            const id = this.dataset.offerId;
+            fetch(Routing.generate('offer_accept', {'id': id}), {
+                'method': 'POST'
             })
-    })
-});
-
-const declineBtns = document.querySelectorAll('.decline-btn');
-declineBtns.forEach((btn) => {
-    btn.addEventListener('click', function () {
-        const id = this.dataset.offerId;
-        fetch(Routing.generate('offer_decline', {'id': id}), {
-            'method': 'POST'
+                .then(res => res.json())
+                .then(offers => {
+                    offers.forEach(declineOffer(id));
+                    const card = document.querySelector(`div[data-offer-id="${id}"]`);
+                    console.log(card);
+                    const buttons = card.querySelector('.buttons');
+                    card.removeChild(buttons);
+                    const p = document.createElement('p');
+                    p.innerText = 'offre accepté';
+                    p.classList.add('text-right');
+                    p.classList.add('font-bold');
+                    p.classList.add('text-green-light');
+                    p.classList.add('uppercase');
+                    card.appendChild(p);
+                })
         })
-            .then(() => declineOffer(id) )
-    })
-});
+    });
 
-const declineOffer = function(id){
-    const card = document.querySelector(`div[data-offer-id="${id}"]`);
-    card.style.opacity = 0.5;
-    const buttons = card.querySelector('.buttons');
-    card.removeChild(buttons);
-};
+    const declineBtns = document.querySelectorAll('.decline-btn');
+    declineBtns.forEach((btn) => {
+        btn.addEventListener('click', function () {
+            const id = this.dataset.offerId;
+            fetch(Routing.generate('offer_decline', {'id': id}), {
+                'method': 'POST'
+            })
+                .then(() => declineOffer(id) )
+        })
+    });
+
+    const declineOffer = function(id){
+        const card = document.querySelector(`div[data-offer-id="${id}"]`);
+        card.style.opacity = 0.5;
+        const buttons = card.querySelector('.buttons');
+        card.removeChild(buttons);
+    };
+
+}
