c57c761f0369e8991ecd0a5afcb25e7e638ea281
Alex Tea <alextea2@gmail.com>
2019-06-18 23:05:36 +0200

    missing files

---
+++ assets/js/toggle_filter.js
@@ -0,0 +1,23 @@
+const routes = require('../../public/js/fos_js_routes.json');
+import Routing from '../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min.js';
+
+Routing.setRoutingData(routes);
+
+const btn = document.querySelector('.toggle-filter');
+const header = document.querySelector('header');
+btn.addEventListener('click', function (e) {
+  const filtersContainer = document.querySelector('.search-filters');
+  if(filtersContainer){
+    filtersContainer.style.display = 'none';
+    return;
+  }
+  console.log('clicked');
+  fetch(Routing.generate('app_security_filters'))
+  .then(res => res.text())
+  .then(html => {
+      const doc = new DOMParser().parseFromString(html, "text/html");
+      const container = doc.querySelector('.search-filters');
+      header.appendChild(container);
+  })
+  .catch(error => console.log(error));
+});
